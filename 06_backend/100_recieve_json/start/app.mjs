57d87a0c65090da02c30b6769c64815f1550a666
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-01 20:51:48 +0900

    サーバー側でJSONを受け取る

--- 06_backend/100_recieve_json/start/app.mjs
+++ 06_backend/100_recieve_json/start/app.mjs
@@ -4,6 +4,9 @@ import express from 'express';
 const PORT = 8080;
 const app = express();
 
+//サーバー側でJSONを受け取るために
+app.use(express.json());
+
 app.get('/', function (req, res) {
   res.send(`
     <form action="/result" method="POST">
@@ -11,11 +14,33 @@ app.get('/', function (req, res) {
       <input type="text" name="description">
       <input type="submit">
     </form>
+    <script>
+    const formEl = document.querySelector('form');
+    formEl.onsubmit = function (event) {
+        event.preventDefault();
+        const title = formEl[0].value;
+        const desc = formEl[1].value;
+        
+        const data = {
+            title,
+            desc
+        };
+        
+        fetch('/result', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }, 
+            body: JSON.stringify(data)
+        })
+    }
+</script>
     `);
 });
 
 app.post('/result', function (req, res) {
   const params = req.body;
+  //フロントエンド側から送られたbodyの内容（JSON）を表示
   console.log(params);
 });
 
