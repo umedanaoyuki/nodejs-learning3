5310aebc4a7e8330d86526861337fe23133daba2
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-01 20:34:24 +0900

    res.jsonで確実にJSONを追加する

--- 06_backend/090_json/start/app.mjs
+++ 06_backend/090_json/start/app.mjs
@@ -4,10 +4,13 @@ import express from 'express';
 const PORT = 8080;
 const app = express();
 
+
 app.get('/', function(req, res) {
-    //JSONで表示させる
-    res.send({message: 'hello'});
-})
+    //JSONで表示させる(自動的にJSON stringifyにする)
+    res.json({message: 'hello', number: 1, array: [
+        'banana', 'orange', 1
+        ]});
+});
 
 app.listen(PORT, function() {
     console.log(`Server start: http://localhost:${PORT}`)
