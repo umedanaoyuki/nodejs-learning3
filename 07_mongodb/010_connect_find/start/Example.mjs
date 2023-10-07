4a9cf6fa854aaf808d15012674544bc4626f6d91
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-07 23:17:09 +0900

    mongoDB接続のための設定

--- 07_mongodb/010_connect_find/start/Example.mjs
+++ 07_mongodb/010_connect_find/start/Example.mjs
@@ -3,27 +3,27 @@ env.config();
 
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGO_URI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+    useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1
 });
 
-async function run() {
+async function getCollection() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("books").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        const db = client.db("bookshelf");
+        return db.collection("books");
+    } catch {
+        client.close();
     }
 }
-run().catch(console.dir);
 
+getAllBooks();
+async function getAllBooks() {
+    const col = await getCollection();
+    const cursor = col.find();
+    const result = await cursor.toArray();
+    console.log(result);
+
+    await client.close();
+}
