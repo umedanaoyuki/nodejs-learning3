aa5666f1302145d4c3fbdae22fb5b28aacdcfa48
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-07 21:45:10 +0900

    mongoDB接続のための設定

--- 07_mongodb/010_connect_find/start/Example.mjs
+++ 07_mongodb/010_connect_find/start/Example.mjs
@@ -1,2 +1,29 @@
 import env from "dotenv";
+env.config();
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
